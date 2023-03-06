a84dbee0068b7da6957fd31ff482bac0554a9c9c
elexbeats <skerdi.elezi@live.com>
2023-03-06 15:02:49 +0100

    homepage prototype

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
       <h1>Car-Fix Reservation System</h1>
     </div>
   );
