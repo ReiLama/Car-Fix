6928b417b10e9466071025a11b6ffadbe2a49973
Niko <113851733+nikosobi@users.noreply.github.com>
2023-03-06 16:02:28 +0100

    deleted side bar, added navbar

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import ProviderDetails from "./components/Providers/ProviderDetails";
 import Providers from "./components/Providers/Providers";
-import SideBar from "./components/SideBar/SideBar";
 import Homepage from "./components/Homepage/Homepage";
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <div className="navbar">
-          <Homepage />
-          <h1>Car-Fix Reservation System</h1>
-        </div>
-        <SideBar />
-        <div className="body">
+        <Homepage />
           <Routes>
+            {/* <Route path="/" element={<Services />}></Route> */}
             <Route path="/providers" element={<Providers />}></Route>
-            <Route
-              path="/provider_details/:id"
-              element={<ProviderDetails />}
-            ></Route>
+            <Route path="/provider_details/:id" element={<ProviderDetails />}></Route>
+            {/* <Route path="/reservation/provider/:id" element={<Reservation />}></Route> */}
           </Routes>
-        </div>
       </div>
     </Router>
   );
