c74268d25f3eae75841861f2e40b840b6a518fd5
elexbeats <skerdi.elezi@live.com>
2023-03-06 15:22:40 +0100

    added navbar on top of the page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
-
-
+import ProviderDetails from "./components/Providers/ProviderDetails";
+import Providers from "./components/Providers/Providers";
+import SideBar from "./components/SideBar/SideBar";
 import Homepage from "./components/Homepage/Homepage";
 function App() {
   return (
     <Router>
       <div className="App">
         <div className="navbar">
+          <Homepage />
           <h1>Car-Fix Reservation System</h1>
         </div>
         <SideBar />
         <div className="body">
           <Routes>
             <Route path="/providers" element={<Providers />}></Route>
-            <Route path="/provider_details/:id" element={<ProviderDetails />}></Route>
+            <Route
+              path="/provider_details/:id"
+              element={<ProviderDetails />}
+            ></Route>
           </Routes>
         </div>
       </div>
