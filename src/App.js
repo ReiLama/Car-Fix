76b972fc2235060b523446fcd79f623cfb0563bb
Klajdlila <klajdililaa@gmail.com>
2023-03-10 17:55:05 +0100

    shtova kontakt form

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import "./App.css";
 import ProviderDetails from "./components/Providers/ProviderDetails";
 import Providers from "./components/Providers/Providers";
 import Homepage from "./components/Homepage/Homepage";
+import ContactForm from "./features/ContactForm";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Homepage />
-          <Routes>
-            {/* <Route path="/" element={<Services />}></Route> */}
-            <Route path="/providers" element={<Providers />}></Route>
-            <Route path="/provider_details/:id" element={<ProviderDetails />}></Route>
-            {/* <Route path="/reservation/provider/:id" element={<Reservation />}></Route> */}
-            {/* <Route path="/MyReservations" element={<MyReservations />}></Route> */}
-            {/* <Route path="/Reservation" element={<ReservationDetails />}></Route> */}
-            {/* <Route path="/MyCars" element={<MyCars />}></Route> */}
-            {/* <Route path="/CarDetails" element={<CarDetails />}></Route> */}
-            {/* <Route path="/Profile" element={<Profile />}></Route> */}
-            {/* <Route path="/Contact" element={<Contact />}></Route> */}
-            {/* <Route path="/LogIn" element={<LogIn />}></Route> */}
-            {/* <Route path="/Registration" element={<Registration />}></Route> */}
-          </Routes>
+        <Routes>
+          {/* <Route path="/" element={<Services />}></Route> */}
+          <Route path="/providers" element={<Providers />}></Route>
+          <Route
+            path="/provider_details/:id"
+            element={<ProviderDetails />}
+          ></Route>
+          <Route path="/contact" element={<ContactForm />}></Route>
+          {/* <Route path="/reservation/provider/:id" element={<Reservation />}></Route> */}
+          {/* <Route path="/MyReservations" element={<MyReservations />}></Route> */}
+          {/* <Route path="/Reservation" element={<ReservationDetails />}></Route> */}
+          {/* <Route path="/MyCars" element={<MyCars />}></Route> */}
+          {/* <Route path="/CarDetails" element={<CarDetails />}></Route> */}
+          {/* <Route path="/Profile" element={<Profile />}></Route> */}
+          {/* <Route path="/Contact" element={<Contact />}></Route> */}
+          {/* <Route path="/LogIn" element={<LogIn />}></Route> */}
+          {/* <Route path="/Registration" element={<Registration />}></Route> */}
+        </Routes>
       </div>
     </Router>
   );
---
+++ src/features/ContactForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function ContactForm() {
+  return (
+    <Container>
+      <Row className="mb-5 mt-3">
+        <Col lg="8"></Col>
+      </Row>
+    </Container>
+  );
+}
