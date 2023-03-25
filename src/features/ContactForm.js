a835e34af98eb6f1939b16933e5b0e9305cb71f7
Klajdlila <klajdililaa@gmail.com>
2023-03-25 10:42:43 +0100

    fix

--- src/features/ContactForm.js
+++ src/features/ContactForm.js
@@ -1,58 +1,64 @@
 import React from "react";
 import { Container, Button, TextField } from "@mui/material";
+import { Col, Row } from 'antd';
 import "./contact-form.css";
 import mapimg from "../image/Googlemap-img.jpg";
-import { display } from "@mui/system";
 
 export default function ContactForm() {
   return (
     <Container>
-      <h1>Contact us!</h1>
-      <p>
-        Please fill all the required information, and we will get back to you as
-        soon as possible!
-      </p>
-      <form className="contact-form">
-        <TextField
-          id="first-name"
-          label="First name"
-          variant="standard"
-          fullWidth
-          margin="dense"
-        />
-        <TextField
-          id="last-name"
-          label="Last name"
-          variant="standard"
-          fullWidth
-          margin="dense"
-        />
-        <TextField
-          id="email"
-          label="Email"
-          variant="standard"
-          fullWidth
-          margin="dense"
-        />
-        <TextField
-          id="phone-number"
-          label="Phone number"
-          variant="standard"
-          fullWidth
-          margin="dense"
-        />
-        <TextField
-          id="message"
-          label="Leave a message"
-          variant="standard"
-          fullWidth
-          margin="dense"
-        />
-        <Button className="contact-form-btn" margin="dense" variant="contained">
-          Send
-        </Button>
-      </form>
-      <img src={mapimg}></img>
+      <Row>
+        <Col span={12}>
+          <h1>Contact us!</h1>
+          <p>
+            Please fill all the required information, and we will get back to you as
+            soon as possible!
+          </p>
+          <form className="contact-form">
+            <TextField
+              id="first-name"
+              label="First name"
+              variant="standard"
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              id="last-name"
+              label="Last name"
+              variant="standard"
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              id="email"
+              label="Email"
+              variant="standard"
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              id="phone-number"
+              label="Phone number"
+              variant="standard"
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              id="message"
+              label="Leave a message"
+              variant="standard"
+              fullWidth
+              margin="dense"
+            />
+            <Button className="contact-form-btn" margin="dense" variant="contained">
+              Send
+            </Button>
+          </form>
+        </Col>
+        <Col span={12}>
+          <img className="imag" src={mapimg}></img>
+        </Col>
+      </Row>
     </Container>
   );
 }
